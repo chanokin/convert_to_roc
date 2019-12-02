423b9fc3471e7661572e2cb221ccfc8dfdff8ab4
Gary <chanokin@gmail.com>
2019-12-02 12:22:51 +0000

    force hombrew on large kernels

--- focal/focal.py
+++ focal/focal.py
@@ -109,8 +109,9 @@ class Focal():
         max_val = big_image[max_coords]
         
         if max_val == numpy.inf or max_val == -numpy.inf or max_val == numpy.nan:
-          sys.stderr.write("\nWrong max value in FoCal!")
-          break
+            sys.stderr.write("\n\n--- Wrong max value in FoCal!\n")
+            sys.stderr.flush()
+            break
         
         # translate coordinates from the big image's to a single image coordinates
         if max_coords[0] < height and max_coords[1] < width:
@@ -154,20 +155,14 @@ class Focal():
     convolved_img = {}
 
     for cell_type in range(num_kernels):
-      kh, kw = self.kernels.full_kernels[cell_type].shape
-      if img_width < kw or img_height < kh:
-        force_homebrew = True
-      else:
-        force_homebrew = False
-      
-      is_off_centre = cell_type in [0, 2]
-      c = self.convolver.dog_sep_convolution(img, self.kernels[cell_type], 
-                                             cell_type,
-                                             originating_function="filter",
-                                             force_homebrew=force_homebrew,
-                                             is_off_center=is_off_centre,
-                                             mode='full')
-      convolved_img[cell_type] =  c
+        kh, kw = self.kernels.full_kernels[cell_type].shape
+        force_homebrew = ((img_width < (kw//2)) or (img_height < (kh//2)))
+
+        is_off_centre = cell_type in [0, 2]
+        c = self.convolver.dog_sep_convolution(img, self.kernels[cell_type], cell_type,
+                originating_function="filter", force_homebrew=force_homebrew,
+                is_off_center=is_off_centre, mode='full')
+        convolved_img[cell_type] =  c
 
     return convolved_img
 
